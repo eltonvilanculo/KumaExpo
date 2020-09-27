86116185a1a25d1db86043a92b67cf60524b49b5
Elton Vilanculos <elltonvillas10@gmail.com>
2020-09-27 23:07:07 +0200

    Fetch data

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { StyleSheet ,View} from 'react-native';
+import { StyleSheet ,View,Text , ScrollView} from 'react-native';
 import { ListItem  } from 'react-native-elements'
 import { Header ,Item ,Icon , Input, Content} from "native-base";
 
 
 
 export default class MainScreen  extends React.Component{
+constructor(props){
+    super(props)
+    this.state = {
+        data: [] ,
+        isLoading:true
+
+    }
+
+}
+  
+     async  componentDidMount(){
+
+        try{
+        const response =  await fetch('https://api.npoint.io/bce18030bdcecac96f0d');
+        const data = await response.json()
+        this.setState({
+            data:data , 
+            isLoading:false
+        })
+    } catch(error){
+        console.log('FETCHERROR', error)
+    }
+        
+       }
+
+    
 
 
 
     render(){
-        
+        const {data ,isLoading} = this.state ; 
+      
+       
         return(
 
            
@@ -24,22 +52,34 @@ export default class MainScreen  extends React.Component{
                                       <Input placeholder="Pesquisar" />
                                   </Item>
                                
-                             
+                         
             </Header>
-          {/* <ListItem key={7777} bottomDivider>
-          
-          <ListItem.Content>
-            <ListItem.Title>Palavra em pt</ListItem.Title>
-            <ListItem.Subtitle>Wronga</ListItem.Subtitle>
-          </ListItem.Content>
-          <ListItem.Chevron />
-          </ListItem> */}
+      
+            <ScrollView>
+        {data?data.map(item=>(<DataItem key={item.id} pt={item.port} rng={item.ronga} />)):<Text>hhh</Text>}  
+        </ScrollView>
             </View>
+            
         )
     }
 }
 
 
+const DataItem = ({id,pt , rng })=>(
+    <ListItem key={id} bottomDivider>
+          
+    <ListItem.Content>
+<ListItem.Title>{pt}</ListItem.Title>
+      
+<ListItem.Subtitle>{rng}</ListItem.Subtitle>
+   
+    </ListItem.Content>
+   
+    <ListItem.Chevron />
+    
+    </ListItem>
+
+)
 
 // export default () => (
 //   <ApplicationProvider {...eva} theme={eva.light}>
